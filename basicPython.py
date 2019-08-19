75aca18f6ec229416a28c1910c41991b00242656
Ariel Shevah <arielshevah@Ariels-MacBook-Pro.local>
2019-08-18 20:02:28 -0400

    z

--- basicPython.py
+++ basicPython.py
@@ -18,3 +18,35 @@ stuff.insert(stuff.__len__(), "e")
 # prints last elem
 # list can be multiple types
 print((stuff))
+
+
+def loops():
+    stuff = "hello my name is sam nice to meet you".split()
+    newstuff = []
+    print(stuff)
+    # added in reverse
+    while stuff:
+        newstuff.append(stuff.pop())
+
+    newstuff.reverse()
+    print(newstuff)
+    while 'sam' in newstuff:
+        newstuff.remove('sam')
+
+    print(newstuff)
+
+loops()
+
+
+def sampleInput():
+    dic = {}
+    while dic.__len__() < 10:
+        name = input("\nwhat is your name?")
+        response = input("\nhow did you find your service?")
+        dic[name] = response
+        print("will keep repeating until 10 recipients have been tended to")
+        print("current number: " + str(dic.__len__()))
+    print(dic)
+
+
+sampleInput()
